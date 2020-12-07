ce7591472d9d593d95947dc89180f2c1e1048960
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-12-07 00:14:24 -0300

    observations texts

--- src/components/ImagesDisplay/index.js
+++ src/components/ImagesDisplay/index.js
@@ -6,9 +6,22 @@ const Form = ({ images }) => {
 
   return (
     <div className="imagesWrapper">
-        {images.length === 0 && (<p>...</p>)}
+        {images.length === 0 && (
+          <div className="observationsWrapper">
+            <h2>Some observations</h2>
+            <div>
+              <p>This app is still on development, so keep in mind that: </p>
+              <ul>
+                <li>It doesn't work for photos from private accounts;</li>
+                <li>In case of multiple photos in the same post, only the first one will be shown here;</li>
+                <li>This process can take a while, something about 5 to 30 seconds;</li>
+                <li>If it takes more than 30 seconds, close and try again later.</li>
+              </ul>
+            </div>
+          </div>
+        )}
 
-        {images.src == 'Loading' && (
+        {images.src === 'Loading' && (
           <div className="loadingWrapper">
             <div className="title">
               <h2>Loading</h2>
